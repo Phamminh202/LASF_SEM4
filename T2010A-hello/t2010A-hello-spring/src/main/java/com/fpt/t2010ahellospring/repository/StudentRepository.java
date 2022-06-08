64337c78a0ddeff42f17dc5cbc7427e914308fbf
Phamminh202 <73330638+Phamminh202@users.noreply.github.com>
2022-06-08 22:58:56 +0700

    Complete CRU product and student

--- T2010A-hello/t2010A-hello-spring/src/main/java/com/fpt/t2010ahellospring/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fpt.t2010ahellospring.repository;
-
-import com.fpt.t2010ahellospring.entity.Student;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student,String> {
-}
